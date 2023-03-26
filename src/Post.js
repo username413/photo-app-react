8f3d984dd2508832fdf7562a5fb8ef63f6e1d079
md <email@domain>
2023-03-26 00:05:30 -0400

    aside css stuff

--- src/Post.js
+++ src/Post.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Post({ post }) {
     return (
-        <div className="card">
-            <div key={post.id}>{post.user.username}</div>
+        <div className="card" key={post.id}>
+            <div>
+                {post.user.username}
+                <i class="fa-solid fa-ellipsis"></i>
+            </div>
             <div>
                 <img src={post.image_url} alt="post"></img>
             </div>
